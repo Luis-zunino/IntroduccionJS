8add820d349b30093b1611188b1773c81a260ccc
Luis_Zunino <zuninoluis01@gmail.com>
2021-02-06 14:29:36 -0300

    Create 02.js
    variable let y las similutudes con var

---
+++ JS/02.js
@@ -0,0 +1,24 @@
+//variables con let, no tiene diferencia con respecto a var
+
+let producto = "audifonos"; //iniciar variable y se le asigna el valor
+let disponible; // se inicia la variable pero sin valor
+producto = true; // Reasignando valor de la variable
+
+let producto1 = "computadora",
+  disponible1 = true,
+  categoria = "computadoras";
+
+//condiciones para escribir variables
+
+// let 99variable = "no"; no puede iniciar con nuemros, pero si finalizar
+// let -variable = "no"; no puede iniciar con guion medio, pero si con guion bajo
+// let _variable = "si";
+
+//Estilos de variables
+
+let nombre_producto; //underscore
+let nombreProducto; //Camelcase
+let NombreProducto; //Pascal Case
+let nombreproducto; // Lowel Case
+
+//minúsculas y mayúsculas importan en javascript
