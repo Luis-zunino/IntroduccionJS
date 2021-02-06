1d943de53938b8637274e785e3cc762ae2af79a1
Luis_Zunino <zuninoluis01@gmail.com>
2021-02-06 17:19:43 -0300

    se ven las distintas operaciones que se pueden hacer con el objeto math

---
+++ JS/05numeros.js
@@ -0,0 +1,18 @@
+//numeros
+
+//+ suma, - resta, / division, * producto, % modulo
+
+//el objeto math
+let resultado;
+resultado = Math.pi;
+resultado = Math.round(2.5); //redondeo clasico
+resultado = Math.ceil(2.9); //redondeo siempre para arriba
+resultado = Math.floor(2.1); // siempre dedondea para abajo
+resultado = Math.sqrt(144); //raiz cuadrada
+resultado = Math.abs(-10); //valor absoluto
+resultado = Math.min(1, 5, 13, 45, 2); //minimo de los valores
+resultado = Math.max(1, 5, 13, 45, 2); //maximo de los valores
+resultado = Math.random(); //lanza valores random menores a 1
+resultado = Math.flor(Math.random() * 10); //lanza valores random mayores a 1
+
+console.log(resulado);
