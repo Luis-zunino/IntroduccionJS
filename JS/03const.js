e9def6e9f098c0a3b10b25de83f1ce6b1bffe8ea
Luis_Zunino <zuninoluis01@gmail.com>
2021-02-06 15:27:25 -0300

    Create 03const.js
    variable const y sus dos diferencias con respecto a let

---
+++ JS/03const.js
@@ -0,0 +1,26 @@
+//variables const, tiene diferencia con
+//respecto a let, no se puede inicializar
+//sin valor y no se peude reasignar
+
+const producto = "audifonos"; //si se puede iniciar la variable y asignarle el valor
+const disponible; // no se puede iniciar la variable sin valor
+// producto = true; // No se puede reasignar el valor de la variable
+
+const producto1 = "computadora",
+  disponible1 = true,
+  categoria = "computadoras";
+
+//condiciones para escribir variables
+
+// const 99variable = "no"; no puede iniciar con nuemros, pero si finalizar
+// const -variable = "no"; no puede iniciar con guion medio, pero si con guion bajo
+// const _variable = "si";
+
+//Estilos de variables
+
+const nombre_producto; //underscore
+const nombreProducto; //Camelcase
+const NombreProducto; //Pascal Case
+const nombreproducto; // Lowel Case
+
+//minúsculas y mayúsculas importan en javascript
