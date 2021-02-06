962af9ed4d974b89da29c4dcfb305f6b0e92affe
Luis_Zunino <zuninoluis01@gmail.com>
2021-02-06 14:29:21 -0300

    01.js
    variable var y sus caracteristicas

---
+++ JS/01.js
@@ -0,0 +1,24 @@
+//existen tres tipos de variables, var let y const.
+
+var producto = "audifonos"; //iniciar variable y se le asigna el valor
+var disponible; // se inicia la variable pero sin valor
+producto = true; // Reasignando valor de la variable
+
+var producto1 = "computadora",
+  disponible1 = true,
+  categoria = "computadoras";
+
+//condiciones para escribir variables
+
+// var 99variable = "no"; no puede iniciar con nuemros, pero si finalizar
+// var -variable = "no"; no puede iniciar con guion medio, pero si con guion bajo
+// var _variable = "si";
+
+//Estilos de variables
+
+var nombre_producto; //underscore
+var nombreProducto; //Camelcase
+var NombreProducto; //Pascal Case
+var nombreproducto; // Lowel Case
+
+//minúsculas y mayúsculas importan en javascript
