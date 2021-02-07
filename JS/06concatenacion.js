6e12451e7fb64dae87fd5ab8a270803ea8135db8
Luis_Zunino <zuninoluis01@gmail.com>
2021-02-07 15:41:25 -0300

    06concatenacion.js
    se resume

---
+++ JS/06concatenacion.js
@@ -0,0 +1,13 @@
+//concatenacion y template strings
+
+const nombre = "luis";
+const email = "correo@correo.com";
+
+
+// Concatenacion
+// console.log("Nombre del cliente: " + nombre + " y su correo es: " + email );
+// console.log("Nombre del cliente: " , nombre , " y su correo es: " , email );
+
+
+//Template strings - Strings Literals
+console.log(`Nombre del cliente:  ${nombre} y su correo es: ${email}`);
