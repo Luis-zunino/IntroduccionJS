4cdedabdfaba3ab8ce17cf1bcc44354b3cc37362
Luis_Zunino <zuninoluis01@gmail.com>
2021-02-07 23:11:05 -0300

    Create 08objetos.js
    se arranca a trabajar con objetos

---
+++ JS/08objetos.js
@@ -0,0 +1,18 @@
+//objetos
+
+const nombreProducto = "Monitor 20 Pulgadas";
+const precio = 300;
+const disponible = true;
+
+
+const producto = {
+    nombreProducto: "Monitor 20 Pulgadas",
+    precio: 300,
+    disponible: true
+}
+
+
+console.log(producto);
+console.log(producto.nombreProducto);
+console.log(producto.precio);//de esta manera se accede a los datos
+console.log(producto["precio"]);//asi tambien pero menos usada
