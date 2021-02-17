ecc857a452d361e98c7acc2ea803c3a17e6abe75
Luis_Zunino <zuninoluis01@gmail.com>
2021-02-17 15:04:15 -0300

    metodos_propiedades.js
    se sigue con funciones y arrow functions

--- JS/19metodos_propiedad.js
+++ JS/19metodos_propiedad.js
@@ -1,9 +1,25 @@
 //Metodos de Propiedad
 
 const reproductor = {
-  reproducir: function () {
-    console.log("Reproduciendo Cancion");
+  reproducir: function (id) {
+    console.log(`Reproduciendo Cancion con el ID: ${id}`);
+  },
+  pausa: function () {
+    console.log(`Pausando`);
+  },
+  creandoPlayList: function (nombre) {
+    console.log(`Creando la playlist: ${nombre}`);
+  },
+  reproducirPlaylist: function (nombre) {
+    console.log(`Reproduciendo la playlist: ${nombre}`);
   },
 };
 
-reproductor.reproducir();
+reproductor.borrarCancion = function (id) {
+  console.log(`Eliminando la cancion...${id}`); //agrega propiedades por fuera del objeto
+};
+reproductor.reproducir(3840);
+reproductor.pausa();
+reproductor.borrarCancion(20);
+reproductor.creandoPlayList("Heavy Metal");
+reproductor.reproducirPlaylist("Heavy Metal");
---
+++ JS/21arrow_functions.js
@@ -0,0 +1,11 @@
+//Arrow functions
+//no se puede utilizar arrow functions en la declaracion de la funcion,
+//pero si en la expresion de la funcion
+
+const sumar2 = (n1, n2) => console.log(n1 + n2);
+sumar2(3, 5);
+
+const aprendiendo = (tecnologia) => {
+  console.log(`Aprendiendo: ${tecnologia}`);
+};
+aprendiendo(JavaScript);
