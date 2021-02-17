24bc50e25bcea8a3f1eb17a28f1cbc08d54614dc
Luis_Zunino <zuninoluis01@gmail.com>
2021-02-17 15:42:04 -0300

    Update 21arrow_functions.js
    se convierte todo a arrow functions

--- JS/21arrow_functions.js
+++ JS/21arrow_functions.js
@@ -9,3 +9,40 @@ const aprendiendo = (tecnologia) => {
   console.log(`Aprendiendo: ${tecnologia}`);
 };
 aprendiendo(JavaScript);
+
+//me traje todo lo del archivo 13arrays.js
+
+//Array methods
+const meses = ["enero", "febrero", "marzo", "abril", "mayo"];
+
+const carritos = [
+  { nombre: "monitor de 20 pulgadas", precio: 500 },
+  { nombre: "television de 50 pulgadas", precio: 700 },
+  { nombre: "tablets", precio: 300 },
+  { nombre: "audifono", precio: 200 },
+  { nombre: "teclado", precio: 50 },
+  { nombre: "celular", precio: 500 },
+  { nombre: "bocinas", precio: 300 },
+  { nombre: "laptop", precio: 800 },
+];
+
+//for Each
+meses.forEach((mes) => {
+  if (mes == "marzo") {
+    console.log("Marzo si existe");
+  }
+});
+let resultado;
+
+//Some ideal para arreglos de objetos
+resultado = carritos.some((producto) => producto.nombre === "celular");
+
+//reduce toma todo los numeros y devuelve un resultado
+resultado = carritos.reduce((total, producto) => total + producto.precio, 0);
+
+//filter
+resultado = carritos.filter((producto) => producto.precio > 400);
+
+resultado = carritos.filter((producto) => producto.nombre !== "celular");
+
+console.log(resultado);
